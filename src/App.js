cd2489b0ce091ac38b16560a9e3f67c329591d53
Kiki1203 <85237065+Kiki1203@users.noreply.github.com>
2022-06-22 03:39:31 +0700

    Kiki_setUp_Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from '../src/Components/Navbar/Navbar.jsx';
-import Footer from '../src/Components/Footer/Footer.jsx';
-import Login from '../src/Pages/Login/Login.jsx';
-import Register from '../src/Pages/Register/Register.jsx';
-import ResetPassword from '../src/Pages/ResetPassword/ResetPassword.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Navbar from './Components/Navbar/Navbar.jsx';
+// import Footer from './Components/Footer/Footer.jsx';
+import Login from './Pages/Login/Login.jsx';
+import Register from './Pages/Register/Register.jsx';
+import ResetPassword from './Pages/ResetPassword/ResetPassword.jsx';
+import Cart from './Pages/Cart/Cart.jsx';
+import Checkout from './Pages/Checkout/Checkout.jsx';
+import Confirmation from './Pages/Confirmation/Confirmation.jsx';
+import FormAddress from './Pages/FormAddress/FormAddress.jsx';
+import Home from './Pages/Home/Home.jsx';
+import OrderProcess from './Pages/OrderProcess/OrderProcess.jsx';
+import ProductDetail from './Pages/ProductDetail/ProductDetail.jsx';
+import ProductList from './Pages/ProductList/ProductList.jsx';
+import Profile from './Pages/Profile/Profile.jsx';
+import UploadResep from './Pages/UploadResep/UploadResep.jsx';
+import UploadSuccess from './Pages/UploadSuccess/UploadSuccess.jsx';
 
 function App() {
+  // const location = useLocation();
+
   return (
-    <div>
+    <>
+      {/* {location.pathname === '/login' || location.pathname === '/register' ? null : <Navbar />} */}
       <BrowserRouter>
-        <Navbar />
-        <Footer />
         <Routes>
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/resetpassword" element={<ResetPassword />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/ResetPassword" element={<ResetPassword />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/Checkout" element={<Checkout />} />
+          <Route path="/Confirmation" element={<Confirmation />} />
+          <Route path="/FormAddress" element={<FormAddress />} />
+          <Route path="/OrderProcess" element={<OrderProcess />} />
+          <Route path="/ProductDetail" element={<ProductDetail />} />
+          <Route path="/ProductList" element={<ProductList />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/UploadResep" element={<UploadResep />} />
+          <Route path="/UploadSuccess" element={<UploadSuccess />} />
         </Routes>
       </BrowserRouter>
-    </div>
+      {/* {location.pathname === '/login' || location.pathname === '/register' ? null : <Footer />} */}
+    </>
   );
 }
 
---
+++ src/Pages/Confirmation/Confirmation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Confirmation.css';
+
+const Confirmation = () => {
+  return (
+    <div>
+      <h1>Confirmation</h1>
+    </div>
+  );
+};
+
+export default Confirmation;
