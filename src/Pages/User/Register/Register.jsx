a0d8b0613afcfdec9092f7b83f31e58756139f1b
Kiki1203 <85237065+Kiki1203@users.noreply.github.com>
2022-06-24 22:04:31 +0700

    kiki_register_done

--- src/Pages/User/Register/Register.jsx
+++ src/Pages/User/Register/Register.jsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import { React, useState } from 'react';
+import Axios from 'axios';
+import API_URL from '../../../Helpers/API_URL.js';
+
 import Divider from '@mui/material/Divider';
 import './Register.css';
 import gambar from './../../../Assets/login.svg';
+import Swal from 'sweetalert2';
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 5000,
+  timerProgressBar: true,
+});
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [passwordConf, setPasswordConf] = useState('');
+
+  const onSubmit = () => {
+    try {
+      var data = { username: username, password: password, email: email };
+      // var data2 = passwordConf;
+
+      // Validation
+      // if (username === '' || email === '' || password === '' || data2 === '') throw { message: 'Fill All Data!' };
+      // if (!/\S+@\S+\.\S+/.test(email)) throw { message: 'Email address is invalid' };
+      // if (!username.match('(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{4,}')) throw { message: 'Username should be contain uppercase, number, and symbol' };
+      // if (!password.match('(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])')) throw { message: 'Password should be contain uppercase, number, and symbol' };
+      // if (password.length < 8) throw { message: 'Password weak, please add more characters' };
+      // if (password !== data2) throw { message: 'Password and Confirmation Password doesnt match!' };
+
+      Axios.post(`${API_URL}/user/register`, data)
+        .then((res) => {
+          Toast.fire({
+            title: 'Success!',
+            text: res.data.message,
+            icon: 'success',
+            confirmButtonText: 'Okay!',
+            timer: 1500,
+          });
+          setUsername('');
+          setEmail('');
+          setPassword('');
+          setPasswordConf('');
+        })
+        .catch((err) => {
+          Toast.fire({
+            title: 'Error!',
+            text: err.response.data.message,
+            icon: 'error',
+            confirmButtonText: 'Okay!',
+            timer: 1500,
+          });
+        });
+    } catch (error) {
+      Toast.fire({
+        title: 'Error!',
+        text: error.message,
+        icon: 'error',
+        confirmButtonText: 'Okay!',
+        timer: 1500,
+      });
+    }
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
@@ -26,25 +89,25 @@ const Register = () => {
             <label for="exampleFormControlInput1" className="form-label">
               Name
             </label>
-            <input type="username" className="form-control" id="exampleFormControlInput1" placeholder="Input Username" />
+            <input type="username" className="form-control" id="exampleFormControlInput1" placeholder="Input Username" value={username} onChange={(e) => setUsername(e.target.value)} />
           </div>
           <div className="mb-3">
             <label for="exampleFormControlInput1" className="form-label">
               Email Address
             </label>
-            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
           <div className="mb-3">
             <label for="exampleFormControlInput1" className="form-label">
               Password
             </label>
-            <input type="password" className="form-control" id="exampleFormControlInput1" placeholder="Input Password" />
+            <input type="password" className="form-control" id="exampleFormControlInput1" placeholder="Input Password " value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
           <div className="mb-3">
             <label for="exampleFormControlInput1" className="form-label">
-              Reapet Password
+              Repeat Password
             </label>
-            <input type="password" className="form-control" id="exampleFormControlInput1" placeholder="Input Password Again" />
+            <input type="password" className="form-control" id="exampleFormControlInput1" placeholder="Input Password Again" value={passwordConf} onChange={(e) => setPasswordConf(e.target.value)} />
           </div>
           <div className="form-check">
             <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
@@ -53,7 +116,7 @@ const Register = () => {
             </label>
           </div>
           <div classNameName="mb-3">
-            <button type="button" className="btn btn-danger col-12">
+            <button type="button" className="btn btn-danger col-12 " onClick={() => onSubmit()}>
               Register
             </button>
           </div>
--- src/Pages/User/Verification/Verification.jsx
+++ src/Pages/User/Verification/Verification.jsx
@@ -1,88 +1,110 @@
-import React from 'react';
+import { React, useState, useParams, useEffect } from 'react';
 import './Verification.css';
-import Axios from 'axios'
-import API_URL from "../../../Helpers/API_URL.js"
-import VerifyPage from "../../../Assets/VerifyPage.svg"
+import Axios from 'axios';
+import API_URL from '../../../Helpers/API_URL.js';
+import VerifyPage from '../../../Assets/VerifyPage.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 // SweetAlert
 import Swal from 'sweetalert2';
 const Toast = Swal.mixin({
-    toast: true,
-    position: 'top-end',
-    showConfirmButton: false,
-    timer: 5000,
-    timerProgressBar: true,
-})
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 5000,
+  timerProgressBar: true,
+});
 
 const Verification = () => {
-  const [isRedirect, setIsRedirect] = useState(false)
+  const [isRedirect, setIsRedirect] = useState(false);
 
   let params = useParams();
 
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-      Verification()
-  }, [])
+    Verification();
+  }, []);
 
   const Verification = () => {
-    Axios.patch(`${API_URL}/user/verification`, {}, {headers: {
-        'Authorization': params.token,
-        'Accept' : 'application/json',
-        'Content-Type': 'application/json'
-    }})
-    .then((res) => {
+    Axios.patch(
+      `${API_URL}/user/verification`,
+      {},
+      {
+        headers: {
+          Authorization: params.token,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+      .then((res) => {
         Toast.fire({
-            icon: 'success',
-            title: res.data.message
-        })
-        setIsRedirect(true)
-        localStorage.setItem('myTkn', params.token)
-    })
-    .catch((err) => {
-        setMessage(err.response.data.message)
+          icon: 'success',
+          title: res.data.message,
+        });
+        setIsRedirect(true);
+        localStorage.setItem('myTkn', params.token);
+      })
+      .catch((err) => {
+        setMessage(err.response.data.message);
         Toast.fire({
-            icon: 'error',
-            title: err.response.data.message
-        })
-    })
-}
+          icon: 'error',
+          title: err.response.data.message,
+        });
+      });
+  };
 
-const onResendEmail = () => {
-  let token = localStorage.getItem('myTkn')
+  const onResendEmail = () => {
+    let token = localStorage.getItem('myTkn');
 
-  Axios.post(`${API_URL}/user/resend`, {}, {headers: {
-      'Authorization': token,
-      'Accept' : 'application/json',
-      'Content-Type': 'application/json'
-  }})
-  .then((res) => {
-      Swal.fire({
+    Axios.post(
+      `${API_URL}/user/resend`,
+      {},
+      {
+        headers: {
+          Authorization: token,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+      .then((res) => {
+        Swal.fire({
           title: 'Success!',
           text: 'Check Your Email',
           icon: 'success',
-          confirmButtonText: 'Okay!'
+          confirmButtonText: 'Okay!',
+        });
       })
-  })
-  .catch((err) => {
-      console.log(err)
-  })
-}
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <div className='container'>
-      <div  className="d-lg-block d-md-block d-none" style={{position: 'absolute', height: '109px', left: '-2px',top: '-16px', border: "1px solid blue", width: "1200px"}}>NAVBAR</div>
-        <div className="d-lg-none d-md-none d-block box-navbar-verify">
-            <div className="logo-to-homepage"><FontAwesomeIcon icon={faAngleLeft} /></div>
-            <div className="tulisan-verification">Verification</div>
+    <div className="container">
+      <div className="d-lg-block d-md-block d-none" style={{ position: 'absolute', height: '109px', left: '-2px', top: '-16px', border: '1px solid blue', width: '1200px' }}>
+        NAVBAR
+      </div>
+      <div className="d-lg-none d-md-none d-block box-navbar-verify">
+        <div className="logo-to-homepage">
+          <FontAwesomeIcon icon={faAngleLeft} />
         </div>
-        <div className="logo-verify-page"><img src={VerifyPage} alt=""/></div>
-        <div className="tulisan-oops">Oops...sorry, </div>
-        <div className="tulisan-not-allowed">You are not allowed to access the homepage, don't forget to verify your account first via email or by clicking the button below:</div>
-        <div className="button-resend-email" onClick={() => onResendEmail()}>Resend Email Verification</div>
-      <div className="d-lg-block d-md-block d-none" style={{position: 'absolute', height: '480px', left: '0px',top: '1036px', border: "1px solid blue", width: "1200px"}}>FOOTER</div>
-   </div>
+        <div className="tulisan-verification">Verification</div>
+      </div>
+      <div className="logo-verify-page">
+        <img src={VerifyPage} alt="" />
+      </div>
+      <div className="tulisan-oops">Oops...sorry, </div>
+      <div className="tulisan-not-allowed">You are not allowed to access the homepage, don't forget to verify your account first via email or by clicking the button below:</div>
+      <div className="button-resend-email" onClick={() => onResendEmail()}>
+        Resend Email Verification
+      </div>
+      <div className="d-lg-block d-md-block d-none" style={{ position: 'absolute', height: '480px', left: '0px', top: '1036px', border: '1px solid blue', width: '1200px' }}>
+        FOOTER
+      </div>
+    </div>
   );
 };
 
